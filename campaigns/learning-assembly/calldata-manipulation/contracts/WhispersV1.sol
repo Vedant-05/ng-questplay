7180fbe73ab3f7f075526ed361ccda431b320e5b
Vedant Joshi <vedantj76@gmail.com>
2024-10-18 21:54:53 +0530

    WhispersV1.sol

--- campaigns/learning-assembly/calldata-manipulation/contracts/WhispersV1.sol
+++ campaigns/learning-assembly/calldata-manipulation/contracts/WhispersV1.sol
@@ -6,6 +6,8 @@ contract WhispersV1 {
     /// @notice Read and return the uint256 appended behind the expected calldata.
     function whisperUint256() external pure returns (uint256 value) {
         assembly {
+            // Load the uint256 value from the end of the calldata
+            value := calldataload(0x04)
         }
     }
 
@@ -13,6 +15,21 @@ contract WhispersV1 {
     /// @dev The string is abi-encoded.
     function whisperString() external pure returns (string memory str) {
         assembly {
+           // Allocate some free memory
+            str := mload(0x40)
+            // 4 function selecor 32 for string pointer offset
+            let dataLength := calldataload(0x24)
+
+            // Calculate length of data + length header
+            let totalLength := add(dataLength, 0x20)
+
+            // Copy length and string data into memory
+              calldatacopy(str, 0x24, totalLength)
+
+            // Update free memory pointer
+                let allocated := add(str, totalLength)
+               mstore(0x40, allocated)
+
 
         }
     }
