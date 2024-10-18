b4e0b512b58d3287f1d0ab0755287a8ffda4bbd2
Vedant Joshi <vedantj76@gmail.com>
2024-10-18 16:32:52 +0530

    MaskGenerator yul

--- campaigns/learning-assembly/bits-and-bytes/contracts/MaskGenerator.sol
+++ campaigns/learning-assembly/bits-and-bytes/contracts/MaskGenerator.sol
@@ -14,7 +14,21 @@ contract MaskGenerator {
         bool reversed
     ) public pure returns (uint256 mask) {
         assembly {
+              if gt(add(nBytes, at), 32) {
+                revert(0, 0)
+            }
 
+             // Create a mask of all 1s for the specified nBytes
+            mask := shl(mul(sub(32, nBytes), 8), not(0))
+            
+            // Shift the mask to the correct position
+            mask := shr(mul(sub(32, add(nBytes, at)), 8), mask)
+
+            // If reversed, invert the mask
+            switch reversed
+            case 1 {
+                mask := not(mask)
+            }
         }
     }
 }
