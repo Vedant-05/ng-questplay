8bfbf1b8811b30e78e30e679626a2911c1f2943b
Vedant Joshi <vedantj76@gmail.com>
2024-10-18 16:54:04 +0530

    Scrambled Yul

--- campaigns/learning-assembly/bits-and-bytes/contracts/Scrambled.sol
+++ campaigns/learning-assembly/bits-and-bytes/contracts/Scrambled.sol
@@ -10,7 +10,27 @@ contract Scrambled {
         returns (address rvalue)
     {
         assembly {
+        // desired blue 8 bytes green 6 bytes purple 6 bytes
+        //  8 B, 6 G, 6 P
+        // given 1byte trash 6 byte purple 8 byte trash 6 byte green 2 bytes trash 8 bytes blue part 1 byte trash
+        // 1T,6P,8T,6G,2T,8B,1T
+         let desiredBluePart := shl(mul(23,8),value)
+         desiredBluePart := shr(mul(24,8),desiredBluePart)
 
+         let desiredgreenPart := shl(mul(15,8),value)
+         desiredgreenPart := shr(mul(26,8),desiredgreenPart)
+
+         let desiredPurplePart := shl(mul(1,8),value)
+         desiredPurplePart := shr(mul(26,8),desiredPurplePart)
+
+         rvalue := shl(mul(6,8),desiredBluePart)
+
+         rvalue := or(rvalue,desiredgreenPart)
+
+         rvalue := shl(mul(6,8),rvalue)
+
+         rvalue := or(rvalue,desiredPurplePart)
+            
         }
     }
 }
