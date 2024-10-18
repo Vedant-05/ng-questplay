a37f42539b706b1b6e22cd342ef7e4ce225aa931
Vedant Joshi <vedantj76@gmail.com>
2024-10-18 18:59:58 +0530

    MemoryLayout Yul Bug Fix

--- campaigns/learning-assembly/memory-manipulation/contracts/MemoryLayout.sol
+++ campaigns/learning-assembly/memory-manipulation/contracts/MemoryLayout.sol
@@ -34,9 +34,9 @@ contract MemoryLayout {
         bytes1 value
     ) public pure returns (bytes memory array) {
         assembly {
-            // 1. Read start of free memory
+           // 1. Read start of free memory
             array := mload(0x40)
-            // 2. Record the length of the array
+            // 2. Record the length of the array (in bytes)
             mstore(array, size)
 
             // 3. Initialize next `size` bytes with `value`
@@ -45,11 +45,12 @@ contract MemoryLayout {
                 mstore8(add(dataPtr, i), value)
             }
 
-            // 4. Calculate the number of words needed to store the bytes array
-            // Round up to the nearest 32-byte word boundary
-            let words := div(add(size, 31), 32)
+            // 4. Calculate the total size in bytes, including the length field
+            // and rounding up to the nearest 32-byte word boundary
+            let totalSize := add(0x20, mul(div(add(size, 31), 32), 32))
+            
             // 5. Mark the array memory area as allocated
-            mstore(0x40, add(dataPtr, mul(words, 0x20)))
+            mstore(0x40, add(array, totalSize))
         }
     }
 }
