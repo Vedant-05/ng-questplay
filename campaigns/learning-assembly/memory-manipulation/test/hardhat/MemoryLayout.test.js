fd15d4a19f546e4aa15f38cb67b3b0e61829ec99
Vedant Joshi <vedantj76@gmail.com>
2024-10-18 19:16:28 +0530

    MemoryLayout Yul Bug Fix

--- campaigns/learning-assembly/memory-manipulation/test/hardhat/MemoryLayout.test.js
+++ campaigns/learning-assembly/memory-manipulation/test/hardhat/MemoryLayout.test.js
@@ -4,6 +4,4 @@ const inputs = require("../data/memoryLayout.json");
 describe("Memory Layout (Part 3)", function() {
   testMemoryLayout("Public Test 1", inputs[0]);
   testMemoryLayout("Public Test 2", inputs[1]);
-  testMemoryLayout("Public Test 2", inputs[2]);
-
 });
